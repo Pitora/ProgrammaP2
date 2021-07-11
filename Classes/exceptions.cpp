4e71733ca2a7ee961428df7b020135698f41adea
Pietro <pietro.villatora@gmail.com>
2021-07-11 19:45:59 +0200

    Il file degli errori....

---
+++ Classes/exceptions.cpp
@@ -0,0 +1,24 @@
+#ifndef ERROR
+#define ERROR
+
+
+class err_file {};
+
+class err_sub {};
+
+class err_disequip{};
+
+class err_nodeNotFound{};
+
+class err_notFound{};
+
+class err_wrongType{};
+
+class err_fileNotCreated{};
+class err_fileNotReadable{};
+
+class err_notNew{};
+
+class err_import{};
+
+#endif
